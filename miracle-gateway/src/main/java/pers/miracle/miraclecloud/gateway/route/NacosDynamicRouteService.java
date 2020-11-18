82a8a5bf012886290112645bec861cef980bd96c
cqf <1748672727@qq.com>
2020-11-18 11:23:21 +0800

    gateway动态路由配置

---
+++ miracle-gateway/src/main/java/pers/miracle/miraclecloud/gateway/route/NacosDynamicRouteService.java
@@ -0,0 +1,129 @@
+package pers.miracle.miraclecloud.gateway.route;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+/**
+ * nacos实现动态路由配置
+ * 路由信息以json的格式保存到nacos配置中,如下：
+ * [  {
+ *    "id": "system",
+ *    "order": 0,
+ *    "predicates": [{
+ *                     "args": {
+ *                       "pattern": "/system/**"
+ *                     },
+ *                     "name": "Path"
+ *                   }],
+ *    "uri": "lb://miracle-system"
+ *  }]
+ * @author cqf
+ */
+@Component
+public class NacosDynamicRouteService implements ApplicationEventPublisherAware {
+
+    private String dataId = "gateway-router";
+
+    private String group = "DEFAULT_GROUP";
+
+    @Value("${spring.cloud.nacos.config.server-addr}")
+    private String serverAddr;
+
+    @Autowired
+    private RouteDefinitionWriter routeDefinitionWriter;
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    private static final List<String> ROUTE_LIST = new ArrayList<>();
+
+    @PostConstruct
+    public void dynamicRouteByNacosListener() {
+        try {
+            ConfigService configService = NacosFactory.createConfigService(serverAddr);
+            // 根据dataId，group获取naocs中的配置信息
+            String configInfo = configService.getConfig(dataId, group, 5000);
+            addAndPublishBatchRoute(configInfo);
+            configService.addListener(dataId, group, new Listener() {
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    clearRoute();
+                    try {
+                        List<RouteDefinition> gatewayRouteDefinitions = JSONObject.parseArray(configInfo, RouteDefinition.class);
+                        for (RouteDefinition routeDefinition : gatewayRouteDefinitions) {
+                            addRoute(routeDefinition);
+                        }
+                        publish();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+            });
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clearRoute() {
+        for(String id : ROUTE_LIST) {
+            this.routeDefinitionWriter.delete(Mono.just(id)).subscribe();
+        }
+        ROUTE_LIST.clear();
+    }
+
+    private void addRoute(RouteDefinition definition) {
+        try {
+            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+            ROUTE_LIST.add(definition.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void publish() {
+        this.applicationEventPublisher.publishEvent(new RefreshRoutesEvent(this.routeDefinitionWriter));
+    }
+
+    /**
+     * 批量 添加及发布 路由
+     * @param configInfo 配置文件字符串, 必须为json array格式
+     */
+    private void addAndPublishBatchRoute(String configInfo) {
+        try {
+            clearRoute();
+            List<RouteDefinition> gatewayRouteDefinitions = JSONObject.parseArray(configInfo, RouteDefinition.class);
+            for (RouteDefinition routeDefinition : gatewayRouteDefinitions) {
+                addRoute(routeDefinition);
+            }
+            publish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+}
